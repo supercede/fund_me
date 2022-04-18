a95cca5928f571fb7ff478afdbe668d2a79e27f4
Sijuade Ajagunna <mahyor.sam@gmail.com>
2022-04-18 23:25:09 +0100

    add mainnet fork

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,13 +1,16 @@
 from brownie import network, accounts, config, MockV3Aggregator
-from web3 import Web3
 
 DECIMALS = 8  # Price feed aggregator usually returns 8 decimals
 STARTING_PRICE = 200000000000  # 2000 + 8 decimals
 LOCAL_ENVIRONMENTS = ["development", "ganache-local"]
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 
 
 def get_account():
-    if network.show_active() in LOCAL_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -6,8 +6,12 @@ import pytest
 
 def test_funding_and_withdrawal():
     account = get_account()
+    print(f"account ==> {account}")
     fund_me = deploy_fundme()
-    entrance_fee = fund_me.getEntranceFee()
+    print(f"fund_me ==> {fund_me}")
+    entrance_fee = fund_me.getEntranceFee() + 100
+    print(f"entrance_fee ==> {entrance_fee}")
+
     tx = fund_me.fund({"from": account, "value": entrance_fee})
     tx.wait(1)
     assert fund_me.fundersMap(account.address) == entrance_fee
@@ -21,6 +25,8 @@ def test_only_owner_can_withdraw():
     if network.show_active() not in LOCAL_ENVIRONMENTS:
         pytest.skip("This test only runs in local environments")
     bad_actor = accounts.add()
+    # print(f"bad actor ==> {bad_actor}")
     fund_me = deploy_fundme()
+    # print(f"price {fund_me.getPrice()}")
     with pytest.raises(exceptions.VirtualMachineError):
-        fund_me.withdraw({"from": bad_actor})
+        fund_me.withdraw({"from": bad_actor, "gas_limit": 6721973})
